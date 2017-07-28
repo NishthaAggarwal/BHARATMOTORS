1d6c0446cb180e2558825e74df49073bd025794f
ayushi271992 <ayushi27gupta@gmail.com>
2017-07-28 05:53:33 -0700

    28-july-changes
    json thik krni hai -_-

---
+++ web/php_pages/contact.php
@@ -0,0 +1,25 @@
+<!--  {
+<strong><u><?php echo "BHARAT MOTORS"; ?></u></strong>
+<br><strong><?php echo "Mr. Subhash & Mr. Pankaj" ?></strong>
+<?php echo "Kejriwal House, 1531, S. P. Mukherjee Marg, Near Novelty Cinema, Backside Fire Station, Delhi, India - 110006"; ?>
+<br>
+<strong><?php echo "Phone : " ?></strong><?php echo "+91-11-23912233" ?>
+<br>
+<strong><?php echo "Mobile :  " ?></strong><?php echo "+91-9818256891,+91-9999569276,+91-9810286891" ?>
+<br>
+<strong><?php echo "Email Address : " ?></strong><?php echo " sales@bharatmotorsmb.in" ?>
+<br>
+<strong><?php echo "Web Site : http://www.bharatmotorsindia.in" ?></strong>
+<br>
+<strong><?php echo "Web Page : https://www.exportersindia.com/bharat-moters/" ?></strong>
+} -->
+{
+"Title" : "BHARAT MOTORS",
+"Name"  : "Mr. Subhash & Mr. Pankaj",
+"Address" : "Kejriwal House, 1531, S. P. Mukherjee Marg, Near Novelty Cinema, Backside Fire Station, Delhi, India - 110006",
+"Phone" : "+91-11-23912233",
+"Mobile" : "+91-9818256891,+91-9999569276,+91-9810286891",
+"Email_Address" : "sales@bharatmotorsmb.in",
+"Web_Site" : "http://www.bharatmotorsindia.in",
+"Web_Page" : "https://www.exportersindia.com/bharat-moters/"
+}
---
+++ web/php_pages/demo.php
@@ -0,0 +1,16 @@
+
+<?php  include '../demoPage.html' ;?>
+
+<!-- <script type="text/javascript">
+	function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "contact.php", true);
+  xhttp.send();
+}
+</script> -->
+
