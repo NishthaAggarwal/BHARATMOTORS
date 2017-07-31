5a535f3cc31a785fb60bbe40b2f29cb2c9ae7d57
NishthaAggarwal <nishtha92aggarwal@gmail.com>
2017-07-31 13:03:10 +0530

    Finally contact us working
    added ng-route

--- web/js/bm_Common.js
+++ web/js/bm_Common.js
@@ -1,17 +1,44 @@
-angular.module("BM_Site", [])
+var app=angular.module("BM_Site", ['ngRoute']);
          
-         .controller("contact_about", function($scope,$http) {
-           $scope.Contact_function=function()
-           {
-           	$scope.result="chal ke nhi dera kuch";
-           	$http({
+app.controller("contact_about", function($scope,$http) {
+  //$scope.Contact_function=function(){
+
+      $http({
            		method:'GET',
            		url:'contact1.json'
            	}).success(function(data)
            	{
            		$scope.contact_details=data;
            		//[{'Title':'hi'}];
-
            	}) ;        
-           };
+       //   };
          });
+
+
+
+    
+  
+    // configure our routes
+    app.config(function($routeProvider) {
+        $routeProvider
+
+            // // route for the home page
+            // .when('/', {
+            //     templateUrl : 'pages/home.html',
+            //     controller  : 'mainController'
+            // })
+
+            // route for the about page
+            // .when('/about', {
+            //     templateUrl : 'pages/about.html',
+            //     controller  : 'aboutController'
+            // })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'contact_final.html',
+                controller  : 'contact_about'
+            });
+    });
+
+    
